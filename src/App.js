be529916d21423313e26f39020996118439cef0d
Vinay Jain <vinay.jn7@gmail.com>
2020-02-09 22:56:22 +0530

    Added basic react setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      groups: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch('./data/info.json')
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        groups: json.groups.reverse(),
+      })
+    });
+  }
+
+  render() {
+    var { isLoaded, groups } = this.state;
+    
+    if(!isLoaded) {
+      return <div> Loading...</div>
+    } else {
+      return(
+        <div className="App">
+          {groups.map((group, index) => (
+            <div>
+              <div className="locationMeta">
+              <span className="locationName">{group.name}</span> 
+              <span className="locationDate"> {group.date} </span>
+              </div>            
+            {group.images.map((image, imageIndex) => (
+              <img src={"./data/" + image.files['1x']} width={image.size.width+ "px"} height={image.size.height+ "px"} />
+            ))}
+            </div>
+          ))}            
+        </div>
+      )
+    }    
+  }
 }
 
 export default App;
